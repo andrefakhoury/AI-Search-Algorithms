652f2e434ac6df9d8ebd4ad4d3c1d2215b907e9e
Thiago P <thiagop@usp.br>
2020-10-14 11:22:54 -0300

    added docs to graph

--- include/Graph.hpp
+++ include/Graph.hpp
@@ -19,8 +19,15 @@
 
 const int MAXDIMENSION = 1000;
 
+/** Class Graph contains the maze positions and all search algorithms
+ * 
+ * To represent the maze, an adjacency matrix is used in the form
+ * of a 2D vector.
+ */
+
 class Graph {
 public:
+	/** Position classifies tiles in the maze. */
 	enum Position {
 		VALID = '*',
 		STARTING = '#',
@@ -28,20 +35,82 @@ public:
 		OBJECTIVE = '$',
 		SPECIAL = '!'
 	};
-	Coordinate size{};
-	Coordinate startingPosition{};
-	std::vector<std::vector<char>> matrix;
+
+	Coordinate size{}; // maze size
+	Coordinate startingPosition{}; // starting position in the maze
+	std::vector<std::vector<char>> matrix; // actual maze
 
 	Graph();
+	
+	/** readMatrix is used to read the maze from an input stream.
+	 * 
+	 * The usage of an istream provides the possibility of using a
+	 * specific output stream (for example a file stream), aswell as
+	 * stdin.
+	 * 
+	 * @param in input stream to be read from
+	 */ 
 	void readMatrix(std::istream& in);
+
+	/** generateRandom creates a random maze.
+
+	 * @param seed seed to be used in RNG
+	 */ 
 	void generateRandom(unsigned long seed);
+
+	/** generateRandom creates a random maze.
+	 * 
+	 * @param fixedSize size of the maze
+	 * @param seed seed to be used in RNG
+	 */ 
 	void generateRandom(Coordinate fixedSize, unsigned long seed);
+
+	/** outputMatrix prints the maze to a specific output stream
+	 * 
+	 * The usage of an ostream provides the possibility of using a
+	 * specific output stream (for example a file stream), aswell as 
+	 * stdout.
+	 * 
+	 * @param out output stream to be used
+	 */ 
 	void outputMatrix(std::ostream& out);
+
+	/** checkValid checks whether the graph is valid.
+	 * 
+	 * Validity here is expressed by the maze being solvable.
+	 * 
+	 * @returns feasibility of the maze
+	 */ 
 	bool checkValid();
+
+	/** depthFirstSearch runs a DFS search on the maze
+	 *  
+	 * @returns path from starting to ending position
+	 */
 	std::vector<Coordinate> depthFirstSearch();
+
+	/** breadthFirstSearch runs a BFS search on the maze
+	 *  
+	 * @returns path from starting to ending position
+	 */
 	std::vector<Coordinate> breadthFirstSearch();
+
+	/** bestFirstSearch runs the Dijkstra algorithm on the maze
+	 *  
+	 * @returns path from starting to ending position
+	 */
 	std::vector<Coordinate> bestFirstSearch();
+
+	/** aStar runs the A* algorithm on the maze
+	 *  
+	 * @returns path from starting to ending position
+	 */
 	std::vector<Coordinate> aStar();
+
+	/** hillClimbing runs the Hill Climbing search on the maze
+	 *  
+	 * @returns path from starting to ending position
+	 */
 	std::vector<Coordinate> hillClimbing();
 };
 
